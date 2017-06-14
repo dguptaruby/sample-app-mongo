84945c4f09f4a4fed030e1c1f51c14c356519f51
Dipak <dade.dipakg@gmail.com>
2017-06-14 16:37:41 +0530

    Fixed config settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,10 +61,6 @@ SampleAppMongo::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
   config.log_level = :info
   config.eager_load = true
 end
